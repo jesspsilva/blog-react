fd08d16358c0e0660f9d628204db5a20192735b5
jesspsilva <jps936@hotmail.com>
2021-01-11 18:45:38 +0000

    Handle fetch data errors

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,20 +5,30 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok){
+                throw Error('Could not fetch the requested data');
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
             setIsLoading(false);
+            setError(null);
         })
+        .catch(err => {
+            setIsLoading(false);
+            setError(err.message);
+        });
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{ error }</div>}
             {isLoading && <div>Loading...</div>}
             {blogs &&
                 <BlogList blogs={blogs} title="All blogs" />
