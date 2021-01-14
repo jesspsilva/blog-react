9b603da36368640d7195c89b0b27268190d256c7
Jéssica <jps936@hotmail.com>
2021-01-14 21:24:01 +0000

    Add create new blog page and apply React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import CreateBlog from './components/CreateBlog/CreateBlog';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <CreateBlog />
+            </Route>
+          </Switch>
+        </div> 
       </div>
-    </div>
+    </Router>
   );
 }
 
