f836ece9119cfe6463099ea5d6efa9fcab5a9856
jesspsilva <jps936@hotmail.com>
2021-01-20 19:25:19 +0000

    Create add new blog form

--- src/components/CreateBlog/CreateBlog.js
+++ src/components/CreateBlog/CreateBlog.js
@@ -1,7 +1,58 @@
+import { useState } from 'react';
+
+
 const CreateBlog = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const blog = { title, body, author };
+
+        setIsLoading(true);
+        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsLoading(false);
+        })
+    }
+
     return(
-        <div className="create-blog">
+        <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    type="text" 
+                    required
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                >
+                </textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="paul">Paul</option>
+                </select>
+                {!isLoading && <button>Add Blog</button>}
+                {isLoading && <button disabled>Adding blog...</button>}
+            </form>
         </div>
     );
 }
